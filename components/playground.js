e2e41f929de316993c1c7cd5b8b23d8ac55761b2
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-12-06 00:01:07 +0000

    change to planes

--- components/playground.js
+++ components/playground.js
@@ -4,6 +4,24 @@ import * as THREE from 'three';
 import VirtualScroll from 'virtual-scroll';
 import styles from '../styles/Playground.module.css';
 
+const visibleHeightAtZDepth = (depth, camera) => {
+    // compensate for cameras not positioned at z=0
+    const cameraOffset = camera.position.z;
+    if (depth < cameraOffset) depth -= cameraOffset;
+    else depth += cameraOffset;
+
+    // vertical fov in radians
+    const vFOV = (camera.fov * Math.PI) / 180;
+
+    // Math.abs to ensure the result is always positive
+    return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
+};
+
+const visibleWidthAtZDepth = (depth, camera) => {
+    const height = visibleHeightAtZDepth(depth, camera);
+    return height * camera.aspect;
+};
+
 const init = () => {
     // Element variables
     const container = document.getElementById('container');
@@ -28,15 +46,29 @@ const init = () => {
 
     // Points
     const totalPoints = 8;
-    const radius = 2;
+    const theta = (Math.PI * 2) / totalPoints;
+    const colors = [
+        'white',
+        'yellow',
+        'hotpink',
+        'lightblue',
+        'red',
+        'orange',
+        'purple',
+        'green',
+    ];
+    const radius = 6;
     // const radius = 3;
     const group = new THREE.Group();
     for (let i = 0; i < totalPoints; i++) {
-        const geo = new THREE.SphereGeometry(0.025, 10, 10);
-        const mat = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        // const geo = new THREE.SphereGeometry(0.025, 10, 10);
+        const geo = new THREE.PlaneGeometry(3, 2, 1, 1);
+        const mat = new THREE.MeshBasicMaterial({
+            color: colors[i],
+        });
         const mesh = new THREE.Mesh(geo, mat);
-        const theta = (Math.PI * 2) / totalPoints;
         const angle = theta * i;
+        console.log(angle);
         group.add(mesh);
         mesh.position.set(
             radius * Math.cos(angle),
@@ -45,7 +77,8 @@ const init = () => {
         );
     }
     scene.add(group);
-    group.position.x = 0;
+    // group.position.x = 0;
+    group.position.x = -visibleWidthAtZDepth(0, camera) / 2;
     // group.position.x = -radius * 2;
 
     // Scrolling
@@ -54,15 +87,26 @@ const init = () => {
     let scrollSpeed = 0;
     let scrollTargetSpeed = 0;
     const scroller = new VirtualScroll();
+    console.log(scroller);
     scroller.on((event) => {
         scrollPos = event.y / 4000;
-        scrollSpeed = event.deltaY / 2000;
-        // console.log(event);
+        scrollSpeed = (event.deltaY * theta) / 1000;
+        // console.log(scrollPos);
+        console.log(event.originalEvent);
+        console.log(event.y);
+
+        // always allow scroll, but if position less than half way, scroll back
+        // if position more than halfway, scroll forwards
+        // event.y = 5000;
+    });
+
+    scroller.off((event) => {
+        console.log(event);
     });
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor('#e5e5e5');
+    renderer.setClearColor('#000000');
     renderer.setSize(window.innerWidth, window.innerHeight);
     webglEl.appendChild(renderer.domElement);
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -81,9 +125,18 @@ const init = () => {
         scrollSpeed *= 0.9;
         scrollTargetSpeed += (scrollSpeed - scrollTargetSpeed) * 0.1;
         scrollTargetPos += (scrollPos - scrollTargetPos) * 0.1;
-        group.rotation.z = scrollTargetPos;
         group.rotation.y = scrollTargetSpeed * 4;
         group.position.z = -scrollTargetSpeed * 5;
+        group.rotation.z = scrollTargetPos;
+
+        // if (scrollSpeed < 0.01 && scrollSpeed > -0.01) {
+        //     scrollPos = Math.ceil(scrollPos / theta) * theta;
+        // } else {
+        //     group.rotation.z = scrollTargetPos;
+        //     // scrollPos = 0;
+        // }
+
+        // when scroll speed comes close to 0 (within a threshold), set group.rotation.z to one of the angles (theta * i)
 
         // for (let i = 0; i < group.children.length; i++) {
         //     group.children[i].position.z = -scrollTargetSpeed * 5;

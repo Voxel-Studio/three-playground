5f082bd4e4200439e65e05ed51f1d09910203218
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2023-01-05 10:14:28 +0000

    add contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,12 +2,12 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import styles from '../styles/TitleSection.module.css';
 
-export default function Custom404() {
+export default function Contact() {
     return (
         <div className={styles.container}>
             <Header />
             <div className='wrapper'>
-                <h1 className={styles.h1}>Page not found</h1>
+                <h1 className={styles.h1}>Contact</h1>
                 <div className={styles.line}></div>
             </div>
             <Footer />

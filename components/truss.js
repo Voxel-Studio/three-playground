4e605ea4cb9d52294f1ffc7ab6c177bcf6bb2e4b
LucyRTCodes <lucyrtutton@gmail.com>
2023-10-23 14:48:42 +0100

    update portfolio wheel for mobile

--- components/truss.js
+++ components/truss.js
@@ -2,30 +2,30 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Truss(props) {
-  const { nodes, materials } = useGLTF("/stage_truss_short_v2_-_low_poly.glb");
-  return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes["Material_003-material"].geometry}
-            material={materials.Material_003}
-          >
-            <meshStandardMaterial color="black" />
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes["Material_004-material"].geometry}
-          >
-            <meshStandardMaterial color="black" />
-          </mesh>
-        </group>
-      </group>
-    </group>
-  );
+	const { nodes, materials } = useGLTF("/stage_truss_short_v2_-_low_poly.glb");
+	return (
+		<group {...props} dispose={null}>
+			<group rotation={[-Math.PI / 2, 0, 0]}>
+				<group rotation={[Math.PI / 2, 0, 0]}>
+					<mesh
+						castShadow
+						receiveShadow
+						geometry={nodes["Material_003-material"].geometry}
+						material={materials.Material_003}
+					>
+						<meshStandardMaterial color="#222222" />
+					</mesh>
+					<mesh
+						castShadow
+						receiveShadow
+						geometry={nodes["Material_004-material"].geometry}
+					>
+						<meshStandardMaterial color="#333333" />
+					</mesh>
+				</group>
+			</group>
+		</group>
+	);
 }
 
 useGLTF.preload("/stage_truss_short_v2_-_low_poly.glb");

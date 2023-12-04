414c63a00f267d8992c381993d54225b67b9f4d8
camcgreen <c.c.green@outlook.com>
2023-12-04 10:34:04 +0000

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -1,93 +1,92 @@
-export const loadingTimeMs = 4500;
+export const loadingTimeMs = 4500
 // export const loadingTimeMs = 500;
 
 // THIS NEEDS TO BE CONVERTED TO THE CMS
 export const newsItems = [
   {
-    title: "Lorem ipsum dolor lorem upsleviosa",
-    img: "/news1.jpg",
-    id: "lorem-ipsum-1",
+    title: 'Lorem ipsum dolor lorem upsleviosa',
+    img: '/news1.jpg',
+    id: 'lorem-ipsum-1',
   },
   {
-    title: "Onsite at ExCeL London for ICE 2023",
-    img: "/news2.jpg",
-    id: "onsite-at-excel-london",
+    title: 'Onsite at ExCeL London for ICE 2023',
+    img: '/news2.jpg',
+    id: 'onsite-at-excel-london',
   },
   {
-    title: "Lorem ipsum dolor lorem upsleviosa",
-    img: "/news3.jpg",
-    id: "lorem-ipsum-2",
+    title: 'Lorem ipsum dolor lorem upsleviosa',
+    img: '/news3.jpg',
+    id: 'lorem-ipsum-2',
   },
-];
+]
 
-// THIS NEEDS TO BE CONVERTED TO THE CMS
 export const projectItems = [
   {
-    title: "TikTok Summit",
+    title: 'TikTok Summit',
     image: `/adidas-1.jpeg`,
-    id: "project-1",
+    id: 'project-1',
   },
   {
-    title: "Adidas",
+    title: 'Adidas',
     image: `/tiktok-1.jpeg`,
-    id: "project-2",
+    id: 'project-2',
   },
   {
-    title: "Sweaty Betty",
+    title: 'Sweaty Betty',
     image: `/adidas-1.jpeg`,
-    id: "project-3",
+    id: 'project-3',
   },
   {
-    title: "Invisilign",
+    title: 'Invisalign',
     image: `/tiktok-2.jpeg`,
-    id: "project-4",
+    id: 'project-4',
   },
   {
-    title: "Microsoft*",
+    title: 'Microsoft*',
     image: `/adidas-1.jpeg`,
-    id: "project-5",
+    id: 'project-5',
   },
   {
-    title: "American Express",
+    title: 'American Express',
     image: `/tiktok-1.jpeg`,
-    id: "project-6",
+    id: 'project-6',
   },
   {
-    title: "Chanel or Stone Island",
+    title: 'Chanel or Stone Island',
     image: `/adidas-1.jpeg`,
-    id: "project-7",
+    id: 'project-7',
   },
   {
-    title: "BMW/Mini",
+    title: 'BMW/Mini',
     image: `/tiktok-2.jpeg`,
-    id: "project-8",
+    id: 'project-8',
   },
-];
+]
 
 export const serviceItems = [
   {
-    title: "Live Events",
+    title: 'Live Events',
     image: `/adidas-1.jpeg`,
-    id: "live-events",
+    id: 'live-events',
   },
   {
-    title: "Virtual Events",
+    title: 'Virtual Events',
     image: `/tiktok-1.jpeg`,
-    id: "virtual-events",
+    id: 'virtual-events',
   },
   {
-    title: "Digital",
+    title: 'Digital',
     image: `/adidas-1.jpeg`,
-    id: "digital",
+    id: 'digital',
   },
   {
-    title: "Experiential",
+    title: 'Experiential',
     image: `/tiktok-2.jpeg`,
-    id: "experiential",
+    id: 'experiential',
   },
   {
-    title: "AV Hire",
+    title: 'AV Hire',
     image: `/adidas-1.jpeg`,
-    id: "av-hire",
+    id: 'av-hire',
   },
-];
+]
